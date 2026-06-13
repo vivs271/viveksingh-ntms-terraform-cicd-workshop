b1e565b079318fadd5fdba876b7c158960637a74
ayushi <ayushi@outlook.com>
2026-06-13 10:26:00 +0000

    terraform files added

---
+++ terraformfiles/main.tf
@@ -0,0 +1,17 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "ayushi-rg-cicd-ntms-workshop"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "ayushi-vnet-cicd-workshop"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name = "web-subnet"
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name = azurerm_resource_group.rg.name
+}
